334839c75530671aaf726346606929c25dd50bc3
TomasElia35 <eliagimeneztomas@gmail.com>
2025-12-29 23:44:57 -0300

    [1.0.0]Espulpa{Frontend en proceso}

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Catalogo } from './components/catalogo/catalogo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home }, // Landing Page
+    { path: 'catalogo', component: Catalogo }, // Ejemplo
+    { path: '**', redirectTo: '' } // Cualquier ruta desconocida va al home
+];
